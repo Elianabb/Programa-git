f107ff1d0d67e277552042b5c4599b2fd30c5bf1
Andrea <benavidesandrea602@gmail.com>
2024-07-10 11:17:38 -0500

    se agrego

---
+++ Libreta Andrea Benavides.sql
@@ -0,0 +1,12 @@
+SELECT * FROM libreta.libreta;
+insert into libreta (nombre,domicilio,telefono) values 
+('Alberto Mores', 'Colon 123', '4234567'),
+('Juan Torres', 'Avellaneda 135', '4458787'),
+('Alejandra Erazo', 'Cali', '3221234566'),
+('Sebastian Rios', 'Ipiales', '3156667890'),
+('Luis Enrique', 'La Union', '3167895645'),
+('Sivia Pinal', 'Bogota', '3166764523'),
+('Alberto Mores', 'Pasto', '3123457676');
+
+
+
